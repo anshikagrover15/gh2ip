52335a475e87eed4bf3f004f9d09e05d337b7017
RandomArtist22 <ytanimator123@gmail.com>
2025-08-31 00:43:29 +0530

    Edited the structure of the json

--- myapp/views.py
+++ myapp/views.py
@@ -21,10 +21,8 @@ data_list = []
 
 template_data = {
     "layers": [
-        {"id": "existingAssets", "name": "Existing & Planned Assets", "checked": True},
         {"id": "renewableSources", "name": "Renewable Energy Sources", "checked": True},
         {"id": "demandCenters", "name": "Demand Centers", "checked": True},
-        {"id": "transportLogistics", "name": "Transport Logistics", "checked": True},
         {"id":"landPrices", "name":"Land Price heatmap", "checked": True},
         {"id":"dottedRegion", "name":"Dotted Region", "checked": True}
 
@@ -35,27 +33,17 @@ template_data = {
         {"id": "costOpt", "name": "Cost Optimisation", "value": 90}
     ],
     "map_data": {
-
-        "assets": [
-  ],
         "renewables": [
             # {"id": 1, "type": "Solar", "name": "Bhadla Solar Park", "lat": 27.5333, "lng": 71.9167, "potential": 2245},
             # {"id": 2, "type": "Wind", "name": "Muppandal Wind Farm", "lat": 8.2718, "lng": 77.5358, "potential": 1500}
         ],
         "demand": [
-            {"id": 1, "name": "Industrial Hub - Gujarat", "lat": 23.0225, "lng": 72.5714, "demand": "High"},
-            {"id": 2, "name": "Transport Corridor - Delhi", "lat": 28.7041, "lng": 77.1025, "demand": "Medium"}
+            # {"id": 1, "name": "Industrial Hub - Gujarat", "lat": 23.0225, "lng": 72.5714, "demand": "High"},
+            # {"id": 2, "name": "Transport Corridor - Delhi", "lat": 28.7041, "lng": 77.1025, "demand": "Medium"}
         ],
-        "transport": {
-            "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "geometry": {"type": "LineString", "coordinates": [[70.0577, 22.3733], [72.5714, 23.0225]]}
-            }]
-        },
         "optimisedLocations": [
-            {"lat": 25.4358, "lng": 81.8463, "score": 95, "reason": "High solar potential, proximity to industrial demand."},
-            {"lat": 17.3850, "lng": 78.4867, "score": 88, "reason": "Balanced renewable access and emerging tech hub demand."}
+            # {"lat": 25.4358, "lng": 81.8463, "score": 95, "reason": "High solar potential, proximity to industrial demand."},
+            # {"lat": 17.3850, "lng": 78.4867, "score": 88, "reason": "Balanced renewable access and emerging tech hub demand."}
         ],
         "landPrices":data_list
     },
